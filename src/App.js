5013a8d7a3028cda1377340430b6bf2b2fe70eda
Daniel Li <dl743@cornell.edu>
2018-02-22 23:23:24 -0500

    Add semantic ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Menu } from 'semantic-ui-react';
+import Home from './home/Home';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <p>Test.</p>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <Menu pointing secondary>
+        <Menu.Item name='home' />
+        <Menu.Item name='messages' />
+        <Menu.Item name='friends' />
+      </Menu>
+      <Route exact path='/' component={Home} />
+    </div>
+  </Router>
+)
 
 export default App;
---
+++ src/home/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render () {
+    return (
+      <div>
+        <h2>Clicker</h2>
+        <p>Welcome to the Clicker web app!</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
