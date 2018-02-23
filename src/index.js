040f392448523e307a287e9a8cab51ee747471a9
Daniel Li <dl743@cornell.edu>
2018-02-22 23:32:29 -0500

    Add semantic in package.json and remove index css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import '../semantic/dist/semantic.min.css';
+import './semantic/semantic.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
