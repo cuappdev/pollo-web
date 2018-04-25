e2be44d6e90d8fc136cca19ff3ab8f2d6b8a8358
Annie Cheng <ac962@cornell.edu>
2018-04-25 17:57:41 -0400

    Add UI for Home page navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,43 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Container, Menu } from 'semantic-ui-react';
+import { Header, Menu } from 'semantic-ui-react';
 import Home from './home/Home';
 import './App.css';
 
 class App extends Component {
-  state = { activeItem: 'home' }
+  state = { activeTab: 'CREATED' }
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  handleNavbarTabClick = (e, { name }) => this.setState({ activeTab: name })
+
   render () {
-    const { activeItem } = this.state;
+    const { activeTab } = this.state;
     return (
       <Router>
         <div className='app'>
           <div className='app-header'>
-            <Container text className='navbar'>
-              {/* <Menu text>
-                <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
-              </Menu> */}
-              <h1>Pollo</h1>
-            </Container>
+            <div className='header-section'>
+              <Header as='h1' className='header-title'>Polls</Header>
+              <div className='header-content'>
+                <div className='navbar'>
+                  <Menu pointing secondary className='navbar-menu'>
+                    <Menu.Item
+                      content='Created'
+                      name='CREATED'
+                      active={activeTab == 'CREATED'}
+                      onClick={this.handleNavbarTabClick}
+                    />
+                    <Menu.Item
+                      content='Joined'
+                      name='JOINED'
+                      active={activeTab == 'JOINED'}
+                      onClick={this.handleNavbarTabClick}
+                    />
+                  </Menu>
+                </div>
+                {/* TODO: Add Join Session */}
+              </div>
+            </div>
           </div>
-          <Container text className='app-content'>
-            <Route exact path='/' component={Home} />
-          </Container>
         </div>
       </Router>
     );
