40be2b81f96ea327d853f36ae008fa4e5a7d9f68
Annie Cheng <ac962@cornell.edu>
2018-05-19 14:48:46 -0400

    Add UI for static session components

--- src/home/session/Session.jsx
+++ src/home/session/Session.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Icon } from 'semantic-ui-react';
+import { Icon, Button, Menu } from 'semantic-ui-react';
 import UserSession from './user/UserSession';
 import AdminSession from './admin/AdminSession';
 import io from 'socket.io-client';
@@ -7,44 +7,45 @@ import { hostURL } from '../../utils/constants';
 import './Session.css';
 
 class Session extends Component {
-  socket = io(`${hostURL}/${this.props.session.id}`, {
-    query: {
-      userType: this.props.session.userType
-    }
-  });
 
-  componentDidMount () {
-    this.socket.on('disconnect', () => {
-      this.props.onDisconnect();
-    });
+  state = { activeTab: 'Q & A' }
+
+  handleNavbarTabClick = (e, { name }) => this.setState({ activeTab: name })
+
+  createPoll = () => {
+
   }
-
-  componentWillUnmount () {
-    this.socket.close();
-  }
-
-  leaveSession = () => this.socket.disconnect();
 
   render () {
-    const { userType, code } = this.props.session;
+    const { activeTab } = this.state;
+
+    // FIX: Dummy values for integrating UI
+    var userType = 'admin';
+    var code = 'ABCDEF'
 
     return (
-      <div>
-        <div className='session-status'>
-          {'Session Code: ' + code}
-          <button
-            onClick={this.leaveSession}
-            id='session-leave'
-          >
-            <Icon name='close' />
-            {userType === 'user' ? 'Leave Session' : 'End Session'}
-          </button>
+      <div className='session'>
+        <div className='session-navbar'>
+          <Menu pointing secondary className='navbar-menu'>
+            <Menu.Item
+              content='Q & A'
+              name='Q & A'
+              active={activeTab == 'Q & A'}
+              onClick={this.handleNavbarTabClick}
+            />
+            <Menu.Item
+              content='Review'
+              name='REVIEW'
+              active={activeTab == 'REVIEW'}
+              onClick={this.handleNavbarTabClick}
+            />
+          </Menu>
         </div>
-        <div className='session-content'>
-          {userType === 'user'
-            ? <UserSession socket={this.socket} />
-            : <AdminSession socket={this.socket} />
-          }
+        <Button className='create-poll-button' primary onClick={this.createPoll}>Create a poll</Button>
+        <div className='session-content'></div>
+        <div className='session-info'>
+          <div className='session-name'>Test Class</div>
+          <div className='session-code'>{'Code: ' + code}</div>
         </div>
       </div>
     );
