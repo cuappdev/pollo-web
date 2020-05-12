d330ed5cab3401e75784f9ccbcc12ad7cc1aed41
Matthew Coufal <mjc473@cornell.edu>
2020-05-12 15:45:27 -0700

    Fix constraint issues with login components

--- src/components/LoginCardView/index.tsx
+++ src/components/LoginCardView/index.tsx
@@ -16,18 +16,18 @@ const LoginCardView: React.FunctionComponent<LoginCardViewProps> = ({
     onLogin,
 }) => {
     return (
-        <div className="card">
-            <div className="logo-container">
+        <div className="login-card-view-container">
+            <div className="login-card-view-logo-container">
                 <LogoView type="large-background" />
             </div>
-            <div className="header-text">Welcome to</div>
-            <div className="name-text">Pollo</div>
-            <div className="pronunciation-text">"Poh-loh"</div>
-            <div className="border" />
-            <div className="login-button-container">
+            <div className="login-card-view-header-text">Welcome to</div>
+            <div className="login-card-view-name-text">Pollo</div>
+            <div className="login-card-view-pronunciation-text">"Poh-loh"</div>
+            <div className="login-card-view-border" />
+            <div className="login-card-view-login-button-container">
                 <GoogleLogin
                     buttonText="Sign In with Google"
-                    className="login-button"
+                    className="login-card-view-login-button"
                     clientId={googleClientId}
                     disabled={isLoading}
                     onFailure={onLogin}
