b27687d7ceddc9fe5131c326e9bf658292467541
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-16 03:06:50 +0000

    Deploy cuappdev/pollo-web to github.com/cuappdev/pollo-web.git:gh-pages

---
+++ precache-manifest.2559d6d6e15e70fee8680708ef60c7ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fec4f0ce3ffc70ffc2b82cf9ee18ada",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50a07a8e30170413f354",
+    "url": "/static/css/main.fbf3f0fc.chunk.css"
+  },
+  {
+    "revision": "0b1b23781a070990b8e6",
+    "url": "/static/js/2.ebf24094.chunk.js"
+  },
+  {
+    "revision": "50a07a8e30170413f354",
+    "url": "/static/js/main.53f64aba.chunk.js"
+  },
+  {
+    "revision": "623d47913c2dafa9b7e7",
+    "url": "/static/js/runtime-main.9982cd7a.js"
+  },
+  {
+    "revision": "0e885b0a53a1c5400284b82490652e47",
+    "url": "/static/media/blondelady.0e885b0a.png"
+  },
+  {
+    "revision": "88bbcf14073064aa52d7a89f9e0e3714",
+    "url": "/static/media/blondeman.88bbcf14.png"
+  },
+  {
+    "revision": "91e3fc22ef38b95e56f96507ba0a7496",
+    "url": "/static/media/cornellsso.91e3fc22.png"
+  }
+]);
